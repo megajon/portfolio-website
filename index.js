224cd82958240f5e6f66264fd9ebf6ed3045ff31
Jonathan Seubert <finisher1017@gmail.com>
2020-01-13 16:25:58 -0800

    removed passport and models

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
 const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
-
-
-mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./routes/authRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve production assets
