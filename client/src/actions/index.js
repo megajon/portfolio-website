e8b30fc738a4f66a0490cf3bb346b0be95a90756
Jonathan Seubert <finisher1017@gmail.com>
2020-01-13 20:40:38 -0800

    added Redux actions for stocktwits page

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,48 @@
-import axios from 'axios';
-import { FETCH_USER } from './types';
+import { SEARCH_SYMBOLS, ADD_SYMBOLS , DELETE_SYMBOL, LIST_SYMBOLS } from './types';
 
-const fetchUser = () {
-  axios.get('/api/current_user');
-}
+export const getTwits = () => dispatch => {
+    fetch('/api/stocktwits/get-saved-twits')
+    .then(res => res.json())
+    .then(symbols => dispatch({
+        type: SEARCH_SYMBOLS,
+        payload: symbols
+    }));
+};
+
+// export const getTwitsList = () => dispatch => {
+//     console.log("get twits action");
+//     fetch('/api/stocktwits/list-symbols')
+//     .then(res => res.json())
+//     .then(list => dispatch({
+//         type: LIST_SYMBOLS,
+//         payload: list
+//     }))
+// };
+
+export const addSymbols = symbols => dispatch => {
+    console.log("addsymbols action");
+    fetch(`/api/stocktwits/${symbols}/new-twits`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(symbols => dispatch({
+            type: ADD_SYMBOLS,
+            payload: symbols
+        }))
+};
+
+
+export const deleteSymbol = symbol => dispatch => {
+    console.log("Delete symbol action");
+    fetch(`/api/stocktwits/${symbol}/delete`, {
+        method: 'POST'
+    })
+    .then(res => res.json())
+    .then(symbols => dispatch({
+        type: DELETE_SYMBOL,
+        payload: symbols
+    }));
+};
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,5 @@
-export const FETCH_USER = 'fetch_user';
+export const SEARCH_SYMBOLS = 'SEARCH_SYMBOLS';
+export const ADD_SYMBOLS = 'ADD_SYMBOLS';
+export const DELETE_SYMBOL = 'DELETE_SYMBOL';
+export const LIST_SYMBOLS = 'LIST_SYMBOLS';
 
