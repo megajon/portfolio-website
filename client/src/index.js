2db1a69d93684174d6ee208e119733830384780c
Jonathan Seubert <finisher1017@gmail.com>
2020-01-13 23:40:59 -0800

    Added store to src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import  { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from './App';
 import reducers from './reducers';
 // import store from './store';
 
+const initialState = {};
 
+const middleware = [reduxThunk];
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(
+    reducers, 
+    initialState, 
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>, 
