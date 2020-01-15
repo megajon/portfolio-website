094d11ff8fd625cb5e175e1d0e8929b712a5317a
Jonathan Seubert <finisher1017@gmail.com>
2020-01-15 14:24:27 -0800

    Complete desktop layout v1

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -21,9 +21,9 @@ const Landing = () => {
             <div id="invisible-header"></div>
             <div id="main-navbar">
                 <ul>
-                    <li><button id="profile-button" onClick={scrollToProfile}><i class="fas fa-user fa-10x icon" ></i></button></li>
-                    <li><button id="work-button" onClick={scrollToWork}><i class="fas fa-briefcase fa-10x icon"></i></button></li>
-                    <li><button id="info-button" onClick={scrollToInfo}><i class="fas fa-info-circle fa-10x icon"></i></button></li>
+                    <li><button id="profile-button" onClick={scrollToProfile}><i class="fas fa-user icon" ></i></button></li>
+                    <li><button id="work-button" onClick={scrollToWork}><i class="fas fa-briefcase  icon"></i></button></li>
+                    <li><button id="info-button" onClick={scrollToInfo}><i class="fas fa-info-circle icon"></i></button></li>
                 </ul>
             </div>
             <div id="first">
@@ -41,7 +41,7 @@ const Landing = () => {
             <div id="section2-break" className="section-break"></div>
             <div id="blue" className="section">
                 <div id="blue-content"><h1 className="section-content">Blue Section</h1></div>
-            </div> }
+            </div>
             {/* <SearchForm />
             <TwitsList /> */}
         </>
