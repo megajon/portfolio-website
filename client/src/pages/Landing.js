901ac3ed383de5ba2aa901e305996ffac7b7f51b
Jonathan Seubert <finisher1017@gmail.com>
2020-01-16 21:07:43 -0800

    Backward button moving forward in slideshow

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,59 +1,159 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react'
+import ReactDOM, { render } from 'react-dom';
 
-const Landing = () => {
-    const scrollToProfile = (e) => {
+class Landing extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slideIndex: 1
+        }
+        this.scrollToProfile = this.scrollToProfile.bind(this);
+        this.scrollToWork = this.scrollToWork.bind(this);
+        this.scrollToInfo = this.scrollToInfo.bind(this);
+        this.slideForward = this.slideForward.bind(this);
+        this.slideBackward = this.slideBackward.bind(this);
+
+    }
+
+    componentWillMount() {
+        const renderSlides = async () => {
+            let slides = await document.getElementsByClassName("appSlides");
+            slides[this.state.slideIndex].style.display = "block";
+        }
+        renderSlides();
+    }
+
+
+    scrollToProfile = (e) => {
         e.preventDefault();
         document.querySelector('#section1-break').scrollIntoView({ behavior: 'smooth' });
     }
 
-    const scrollToWork = (e) => {
+    scrollToWork = (e) => {
         e.preventDefault();
         document.querySelector('#section2-break').scrollIntoView({ behavior: 'smooth' });
     }
 
-    const scrollToInfo = (e) => {
+    scrollToInfo = (e) => {
         e.preventDefault();
         document.querySelector('#section3-break').scrollIntoView({ behavior: 'smooth' });
     }
-    return (
-        <>
-            <div id="invisible-header"></div>
-            <div id="main-navbar">
-                <ul>
-                    <li><button id="profile-button" onClick={scrollToProfile}><i class="fas fa-user icon" ></i></button></li>
-                    <li><button id="work-button" onClick={scrollToWork}><i class="fas fa-briefcase  icon"></i></button></li>
-                    <li><button id="info-button" onClick={scrollToInfo}><i class="fas fa-info-circle fa-10x icon"></i></button></li>
-                </ul>
-            </div>
-            <div id="first">
-                <h1 id="first-section"></h1>
-            </div>
-            <div id="title-page" className="section">
-                <div id="title" className="title">
-                    <p>Jonathan C. Seubert</p><br></br>
-                    <p>Creative Technologist</p>
+
+    
+
+    // currentSlide(n) {
+    //     showSlides(slideIndex = n);
+    // }
+
+    showSlides = async (n) =>  {
+        
+        let i;
+        let slides = await document.getElementsByClassName("appSlides");
+        // console.log(slides.length);
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {await this.setState({slideIndex: 1})}
+        if (n < 1) {this.setState({slideIndex: slides.length})}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "none");
+        }
+
+        slides[this.state.slideIndex - 1].style.display = "block";
+    }
+
+    slideForward = (e, n = 1) => {
+        e.preventDefault()
+        console.log("slide forward");
+        this.showSlides(this.state.slideIndex += n);
+    }
+
+    slideBackward = (e, n = -1) => {
+        e.preventDefault()
+        console.log("slide backward");
+        console.log("From plusSlides - n: " + n);
+        this.showSlides(this.state.slideIndex += n);
+    }
+
+
+
+    
+    // showSlides(slideIndex);
+
+    render() {
+
+        return (
+            <>
+                <div id="invisible-header"></div>
+                <div id="main-navbar">
+                    <ul>
+                        <li><button id="profile-button" onClick={this.scrollToProfile}><i className="fas fa-user icon" ></i></button></li>
+                        <li><button id="work-button" onClick={this.scrollToWork}><i className="fas fa-briefcase  icon"></i></button></li>
+                        <li><button id="info-button" onClick={this.scrollToInfo}><i className="fas fa-info-circle fa-10x icon"></i></button></li>
+                    </ul>
                 </div>
-                <div id="link-to-profile">
-                    <a href="#" onClick={scrollToWork}><i class="fas fa-chevron-circle-down next-section"></i></a>
+                <div id="first">
+                    <h1 id="first-section"></h1>
                 </div>
-            </div>
-            <div id="section1-break" className="section-break"></div>
-            <div id="green" className="section">
-                <div><h1 className="section-content">Green Section</h1></div>
-                <div id="green-content"><h1>Green Content</h1></div>
-            </div>
-            <div id="section2-break" className="section-break"></div>
-            <div id="blue" className="section">
-                <div id="blue-content"><h1 className="section-content">Blue Section</h1></div>
-            </div>
-            <div id="section3-break" className="section-break"></div>
-            <div id="yellow" className="section">
-                <div id="yellow-content"><h1 className="section-content">Yellow Section</h1></div>
-            </div>
-            <div id="section4-break" className="section-break"></div>
-        </>
-    )
+                <div id="title-page" className="section">
+                    <div id="title" className="title">
+                        <p>Jonathan C. Seubert</p><br></br>
+                        <p>Creative Technologist</p>
+                    </div>
+                    <div id="link-to-profile">
+                        <a href="#" onClick={this.scrollToWork}><i className="fas fa-chevron-circle-down next-section"></i></a>
+                    </div>
+                </div>
+                <div id="section1-break" className="section-break"></div>
+                <div id="green" className="section">
+                    <div><h1 className="section-content">Green Section</h1></div>
+                    <div id="green-content"><h1>Green Content</h1></div>
+                </div>
+                <div id="section2-break" className="section-break"></div>
+                <div id="blue" className="section">
+                    <div className="appSlides-container">
+                        <div className="appSlides fade">
+                            <div className="panel">
+                                <div className="panel-content">
+                                    <p>one</p>
+                                </div>
+                                <div className="panel-title">Panel Title</div>
+                            </div>
+                        </div>
+                        <div className="appSlides fade">
+                            <div className="panel">
+                                <div className="panel-content">
+                                    <p>two</p>
+                                </div>
+                                <div className="panel-title">Panel Title</div>
+                            </div>
+                        </div>
+                        <div className="appSlides fade">
+                            <div className="panel">
+                                <div className="panel-content">
+                                    <p>three</p>
+                                </div>
+                                <div className="panel-title">Panel Title</div>
+                            </div>
+                        </div>
+                        <a href="#" id="prev" className="prev" onClick={this.slideBackward}>&#10094;</a>
+                        <a href="#" id="next" className="next" onClick={this.slideForward}>&#10095;</a>
+                    </div>
+                    {/* <div>
+                        <span className="dot" onClick={currentSlide(1)}></span> 
+                        <span className="dot" onClick={currentSlide(2)}></span> 
+                        <span className="dot" onClick={currentSlide(3)}></span> 
+                    </div> */}
+                </div>
+                <div id="section3-break" className="section-break"></div>
+                <div id="yellow" className="section">
+                    <div id="yellow-content"><h1 className="section-content">Yellow Section</h1></div>
+                </div>
+                <div id="section4-break" className="section-break"></div>
+            </>
+        )
+    }
 
 }
 
