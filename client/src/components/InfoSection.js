806ee0c55186dcd896e7ceb3cc71b5423e955b01
Jonathan Seubert <finisher1017@gmail.com>
2020-01-19 01:14:25 -0800

    Add social media icons

--- client/src/components/InfoSection.js
+++ client/src/components/InfoSection.js
@@ -2,7 +2,24 @@ import React, {Component} from 'react';
 
 class InfoSection extends Component {
     render() {
-        return <h1 id="info-section">Info Section</h1>
+        return (
+            <>
+                <div id="info-section" className="section">
+                    <div id="social-media-links1">
+                        <a><i class="fab fa-linkedin"></i></a>
+                        <a><i class="fab fa-github"></i></a>
+                        <a><i class="fab fa-facebook-square"></i></a>
+                        <a><i class="fab fa-twitter"></i></a>
+                    </div>
+                    <div id="social-media-links2">
+                        <div id="mail-cv-container">
+                        <a id="mail-icon"><i class="fas fa-envelope"></i></a>
+                        <a id="cv-icon"><i class="fas fa-envelope"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
     }
 }
 
