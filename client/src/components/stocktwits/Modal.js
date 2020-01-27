1b91d2aa71ff078840e3745e30080a0bf4dcbd1d
Jonathan Seubert <finisher1017@gmail.com>
2020-01-27 11:10:59 -0800

    Complete stocktwits modal v1

--- client/src/components/stocktwits/Modal.js
+++ client/src/components/stocktwits/Modal.js
@@ -30,7 +30,16 @@ class Modal extends Component {
                 <div id="stocktwits-modal" className="modal">
                     <div className="modal-content">
                         <span id="hide-modal" className="close" onClick={this.hideModal}>&times;</span>
-                        <p>Some text in the Modal...</p>
+                        <p>
+                            Search Stocktwits is an application that utilizes a public API 
+                            from the website Stocktwits.com, which allows users comment on various
+                            to post comments on various stocks. When a user enters a valid ticker symbol, 
+                            or multiple symbols seperated by a comma, a list of button will appear in the 
+                            body of the application that displays 30 of the most recent twits when pressed. 
+                            Every ten minutes the application will update any symbols in the list with the
+                            latest posts from Stocktwits.com users.
+
+                        </p>
                     </div>
                 </div>
             </>
